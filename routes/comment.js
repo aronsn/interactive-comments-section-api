36e8a85d7df09c48219c0cce1a2e4c3aed4c5c6f
Aron Snekkestad <aron@snekkestad.com>
2024-04-30 01:40:20 +0200

    feat: add 'comments' with post, get and patch routes

--- routes/comment.js
+++ routes/comment.js
@@ -14,10 +14,64 @@ router.route("/")
             console.error(error);
             res.status(500).send("Error fetching comments");
         }
-    }).post(async (req, res) => {
-
-    }).patch(async (req, res) => {
-
     })
+    .post(async (req, res) => {
+        try {
+            let newDocument = {
+                _id: new ObjectId(),
+                content: req.body.content,
+                createdAt: req.body.createdAt,
+                score: req.body.score,
+                user: {
+                    image: {
+                        png: `../public/avatars/image-${req.body.user.username}.png`,
+                        webp: `../public/avatars/image-${req.body.user.username}.webp`
+                    },
+                    username: req.body.user.username,
+                },
+                replies: [],
+            };
+            let collection = await db.collection("comments");
+            let result = await collection.insertOne(newDocument);
+            res.send(result).status(201);
+        } catch (err) {
+            console.error(err);
+            res.status(500).send("Error adding creating comment");
+        }
+    })
+    .patch(async (req, res) => {
+        try {
+            let newArrayObject = {
+                _id: new ObjectId(),
+                content: req.body.content,
+                createdAt: req.body.createdAt,
+                score: req.body.score,
+                replyingTo: req.body.replyingTo,
+                user: {
+                    image: {
+                        png: `../public/avatars/image-${req.body.user.username}.png`,
+                        webp: `../public/avatars/image-${req.body.user.username}.webp`
+                    },
+                    username: req.body.user.username,
+                }
+            };
+
+            const query = { _id: new ObjectId(req.body.id) };
+            const updates = {
+                $set: {
+                    replies: [newArrayObject],
+                },
+            };
+
+            let collection = await db.collection("comments");
+            let result = await collection.updateOne(query, updates);
+            res.send(result).status(204);
+        } catch (err) {
+            console.error(err);
+            res.status(500).send("Error updating record");
+        }
+    })
+
+// TODO - delete route, patch route
 
 export default router;
