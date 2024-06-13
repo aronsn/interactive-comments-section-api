5e4271745ba020fbc2f8a7bf7f6260ffac142413
Aron Snekkestad <aron@snekkestad.com>
2024-06-14 00:40:44 +0200

    feat: improve request validation in POST comments, add validation for data types and undefined variables in the request body and check the content-type is application/json in the request header. Refactor away the mongodb new document parsing. Improve the error messages in GET and POST

--- app/comments/controllers.js
+++ app/comments/controllers.js
@@ -7,33 +7,32 @@ export const getCommentsRequest = async (request, response) => {
         return response.status(200).send(results);
     } catch (error) {
         console.error(`\n${error}`);
-        response.status(500).send(`Error fetching comments: ${error}`);
+        response.status(500).send(`Fetching comments failed. Error: ${error}`);
     }
 }
 
 export const postCommentRequest = async (request, response) => {
     try {
-        let newDocument = {
-            _id: new ObjectId(),
-            content: request.body.content,
-            createdAt: request.body.createdAt,
-            score: request.body.score,
-            user: {
-                image: {
-                    png: `../public/avatars/image-${request.body.user.username}.png`,
-                    webp: `../public/avatars/image-${request.body.user.username}.webp`
-                },
-                username: request.body.user.username,
-            },
-            replies: [],
-        };
+        if (!request.headers['content-type'] && request.headers['content-type'] !== 'application/json') {
+            return response.status(400).send("Content-Type is not correctly set and must be of 'application/json'");
+        }
 
-        const result = await createComment(newDocument);
-        response.status(201).send(result);
+        const { content, createdAt, score, username } = request.body;
+
+        if (content === undefined || createdAt === undefined || username === undefined || score === undefined) {
+            return response.status(400).send('Request is malformed or invalid. The body is missing properties');
+        }
+
+        if (typeof content !== 'string' || typeof createdAt !== 'string' || typeof username !== 'string' || typeof score !== 'number') {
+            return response.status(400).send('Request is malformed or invalid. Check if the data types of the properties provided are correct');
+        }
+
+        const result = await createComment(content, createdAt, score, username);
+        return response.status(201).send(result);
 
     } catch (error) {
         console.error(`\n${error}`);
-        response.status(500).send(`Error creating comment: ${error}`);
+        response.status(500).send(`Creating comment failed. Error: ${error}`);
     }
 }
 
