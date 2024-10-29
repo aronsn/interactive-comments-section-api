5177fbc01395b5bb3814ee944462fad59ca3b200
Aron Snekkestad <aron@snekkestad.com>
2024-10-29 23:53:33 +0100

    feat: implement increment decrement in the edit request handler

--- app/comments/controllers.js
+++ app/comments/controllers.js
@@ -1,4 +1,4 @@
-import { getComments, createComment, createReply, findComment, removeComment, removeReply, updateComment, updateReply } from "./services.js";
+import { getComments, createComment, createReply, findComment, removeComment, removeReply, updateComment, updateReply, updateCommentScore, updateReplyScore } from "./services.js";
 
 export const fetchCommentSection = async (request, response) => {
     try {
@@ -66,9 +66,9 @@ export const editCommentRequest = async (request, response) => {
             return response.status(400).send("Content-Type is not correctly set and must be of 'application/json'");
         }
 
-        const { id, newContent, username } = request.body;
+        const { id, newContent, username, like } = request.body;
 
-        if (id === undefined || newContent === undefined || username === undefined) {
+        if (id === undefined || username === undefined || (newContent === undefined && like === undefined)) {
             return response.status(400).send('Request is malformed or invalid. The body is missing properties');
         }
 
@@ -76,35 +76,60 @@ export const editCommentRequest = async (request, response) => {
             return response.status(400).send('Request is malformed or invalid. The "id" must be a 24 character hex string');
         }
 
-        if (typeof newContent !== 'string') {
+        if ((newContent !== undefined && typeof newContent !== 'string') || (like !== undefined && typeof like !== 'boolean')) {
             return response.status(400).send('Request is malformed or invalid. Check if the data types of the properties provided are correct');
         }
 
         for (let property in request.body) {
-            if (!['id', 'newContent', 'username'].includes(property)) {
+            if (!['id', 'newContent', 'username', 'like'].includes(property)) {
                 return response.status(400).send(`Request is malformed or invalid. "${property}" property is not recognized. `);
             }
         }
 
-        const comment = await findComment(id);
-        const isReply = comment?.replyingTo;
+        if (newContent !== undefined) {
+            const comment = await findComment(id);
+            const isReply = comment?.replyingTo;
 
-        if (!comment) {
-            return response.status(400).send('The provided comment id did not return a match. Comment is either removed or does not exist.');
+            if (!comment) {
+                return response.status(400).send('The provided comment id did not return a match. Comment is either removed or does not exist.');
+            }
+
+            if (username !== comment.user.username) {
+                return response.status(400).send(`This user is not the owner ${comment.user.username} is not the owner of this document`);
+            }
+
+            if (isReply) {
+                let result = await updateReply({ id, newContent });
+                return response.status(204).send(result);
+            }
+
+            let result = await updateComment({ id, newContent });
+            return response.status(200).send(result);
         }
 
-        if (username !== comment.user.username) {
-            return response.status(400).send(`This user is not the owner ${comment.user.username} is not the owner of this document`);
+        if (like !== undefined) {
+            const comment = await findComment(id);
+            const isReply = comment?.replyingTo;
+
+            let number = null;
+            if (like === true) {
+                number = 1;
+            } else {
+                number = -1;
+            }
+
+            if (!comment) {
+                return response.status(400).send('The provided comment id did not return a match. Comment is either removed or does not exist.');
+            }
+
+            if (isReply) {
+                let result = await updateReplyScore({ id, score: number });
+                return response.status(204).send(result);
+            }
+
+            let result = await updateCommentScore({ id, score: number });
+            return response.status(200).send(result);
         }
-
-        if (isReply) {
-            let result = await updateReply({ id, newContent });
-            return response.status(204).send(result);
-        }
-
-        let result = await updateComment({ id, newContent });
-        return response.status(200).send(result);
-
     } catch (error) {
         console.error(`\n${error}`);
         response.status(500).send(`Error: Unable to update comment. ${error.message}`);
