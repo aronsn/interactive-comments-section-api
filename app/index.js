307c7e797969c1da4474668c02bc2258a3209a24
Aron Snekkestad <aron@snekkestad.com>
2025-01-21 23:24:30 +0100

    feat: copy over react app build and serve it from express app

--- app/index.js
+++ app/index.js
@@ -4,6 +4,8 @@ import comments from "./comments/routes.js";
 
 const app = express();
 
+app.use(express.static('dist'));
+
 app.use(cors());
 app.use(express.json());
 app.use("/api/comments", comments);
